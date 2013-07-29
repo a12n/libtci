2e3639d56e619c1601559d16a5434700bc8a0172
Anton Yabchinskiy <arn@bestmx.ru>
2013-07-29 17:09:27 +0400

    C interface is of no use, it'll require linking to libstdc++ anyway

--- tci.cpp
+++ tci.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <stdexcept>
 
-#include "tci.h"
 #include "tci.hpp"
 
 namespace config {
@@ -79,56 +78,3 @@ template int value<int>(const std::string&, const int&);
 template std::string value<std::string>(const std::string&, const std::string&);
 
 } // namespace config
-
-extern "C" {
-
-int
-config_set(const char* path, const char* val)
-{
-    return static_cast<int>(config::set(path, val));
-}
-
-int
-config_set_int(const char* path, int val)
-{
-    return static_cast<int>(config::set(path, val));
-}
-
-const char*
-config_value_r(const char* path, const char* def, char* buf, size_t buf_sz)
-{
-    const char* src;
-    std::string tmp;
-
-    if (config::get(path, tmp)) {
-        src = tmp.c_str();
-    } else {
-        src = def;
-    }
-
-    std::strncpy(buf, src, buf_sz);
-    if (buf_sz > 0) {
-        buf[buf_sz - 1] = '\0';
-    }
-
-    return buf;
-}
-
-const char*
-config_value(const char* path, const char* def)
-{
-    static char buf[512];
-    return config_value_r(path, def, buf, sizeof(buf));
-}
-
-int
-config_int_value(const char* path, int def)
-{
-    int ans;
-    if (config::get(path, ans)) {
-        return ans;
-    }
-    return def;
-}
-
-} // extern "C"
