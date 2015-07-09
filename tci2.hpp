4b85ecb293b0da1d2f0c233c8d6d54b3ad150498
Anton Yabchinskiy <arn@devline.ru>
2015-07-09 12:49:19 +0300

    Import v2 API files

---
+++ tci2.hpp
@@ -0,0 +1,82 @@
+// License: WTFPL (http://www.wtfpl.net/)
+
+#pragma once
+
+#ifndef __TCI2_HPP_INCLUDED__
+#define __TCI2_HPP_INCLUDED__
+
+#include <string>
+
+namespace tci {
+
+// Instantiated only for types:
+//  bool
+//  float
+//  int
+//  std::string
+//  std::vector<bool>
+//  std::vector<float>
+//  std::vector<int>
+//  std::vector<std::string>
+
+class config
+{
+public:
+    explicit
+    config(const std::string& base = "/etc/config");
+
+    template <class T>
+    bool
+    get(const std::string& path, T& ans) const;
+
+    template <class T>
+    void
+    set(const std::string& path, const T& val);
+
+    template <class T>
+    T
+    value(const std::string& path) const;
+
+    template <class T>
+    T
+    value(const std::string& path, const T& def) const;
+
+private:
+    const std::string base_;
+};
+
+//----------------------------------------------------------------------------
+
+extern config default_config;
+
+template <class T>
+bool
+get(const std::string& path, T& ans)
+{
+    return default_config.get<T>(path, ans);
+}
+
+template <class T>
+void
+set(const std::string& path, const T& val)
+{
+    default_config.set<T>(path, val);
+}
+
+template <class T>
+T
+value(const std::string& path)
+{
+    return default_config.value<T>(path);
+}
+
+template <class T>
+T
+value(const std::string& path, const T& def)
+{
+    return default_config.value<T>(path, def);
+}
+
+} // namespace tci
+
+#endif  // __TCI2_HPP_INCLUDED__
